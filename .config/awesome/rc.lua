2f2019944c22c3c7709b1a4d03ad92154802a134
Samuel Bernardo <samuel@atomn550.casa.local>
2014-04-13 00:23:37 +0100

    Inserted battery level info in taskbar and shortcut for realtime info.

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -77,6 +77,67 @@ layouts =
 }
 -- }}}
 
+-- Additional User confs
+battery_widget = widget({ type = "textbox" })
+battery_timer = timer({timeout = 20})
+battery_timer:add_signal("timeout", function()  batteryCheck("BAT0") end)
+battery_timer:start()
+
+function batteryInfo(adapter)
+   local fcur = io.open("/sys/class/power_supply/"..adapter.."/charge_now")    
+   local fcap = io.open("/sys/class/power_supply/"..adapter.."/charge_full")
+   local fsta = io.open("/sys/class/power_supply/"..adapter.."/status")
+   if fcur and fcap and fsta then
+        local cur = fcur:read()
+        local cap = fcap:read()
+        local sta = fsta:read()
+        local battery = math.floor(cur * 100 / cap)
+        if sta:match("Charging") then
+            dir = "^"
+        elseif sta:match("Discharging") then
+            dir = "v"
+        else
+            dir = ""
+        end
+       fcur:close()
+       fcap:close()
+       fsta:close()
+       return {battery,dir}
+   end
+end
+
+function batteryShow(adapter)
+    local batInfos =  batteryInfo(adapter)
+    if batInfos then
+        local battery = batInfos[1]
+        local dir = batInfos[2]
+        infos = " " .. dir .. battery .. "% " 
+    else
+       infos = "absente"
+    end
+      naughty.notify({title = "Battery level",text = infos})
+end
+
+function batteryCheck(adapter)
+    local batInfos = batteryInfo(adapter)
+    if batInfos then
+        local battery = batInfos[1]
+        local dir = batInfos[2]
+        if dir:match("v") and tonumber(battery) < 10 then
+               naughty.notify({ preset = naughty.config.presets.critical,
+                                title = "Batterie Low",
+                                text = " ".. battery .. "% left",
+                                timeout = 30,
+                                font = "Liberation 11", })
+       end
+       infos = " " .. dir .. battery .. "% "
+   else
+       infos = "A/C"
+   end
+   battery_widget.text = infos
+end
+-- }}}
+
 -- {{{ Tags
 -- Define a tag table which hold all screen tags.
 tags = {}
@@ -195,6 +256,7 @@ for s = 1, screen.count() do
         },
         mylayoutbox[s],
         mytextclock,
+				battery_widget,
         s == 1 and mysystray or nil,
         mytasklist[s],
         layout = awful.widget.layout.horizontal.rightleft
@@ -255,6 +317,7 @@ globalkeys = awful.util.table.join(
         end),
 
 		awful.key({ altkey, "Control" }, "l", function () awful.util.spawn("xscreensaver-command -lock") end),
+		awful.key({modkey,           }, "b", function ()  batteryShow("BAT0") end,"Infos battery"),
 
     -- Standard program
 		keydoc.group("Programs"),
